2d18fab488e62e292cfd15573b52d1fab3b6d7f9
PyroFourTwenty <kermitthecreeper@gmail.com>
2020-11-14 18:16:35 +0100

    fixed WorkingDirectory for service

--- src/setup_systemd_service.py
+++ src/setup_systemd_service.py
@@ -1,6 +1,6 @@
 import os
-
-path = os.getcwd()
+from pathlib import Path
+path = Path(os.getcwd()).parent
 service_text = """[Unit]
 Description=No-IP updater
 After=network.target
@@ -14,10 +14,14 @@ Restart=always
 [Install]
 WantedBy=multi-user.target""".format(path)
 
+print('Removing existing service')
 os.system('rm /etc/systemd/system/noipupdater.service')
+print('Writing to service file')
 os.system('echo "{}" >> /etc/systemd/system/noipupdater.service'.format(service_text))
+print('Enabling service')
 os.system('systemctl enable noipupdater.service')
+print('Starting service')
 os.system('systemctl start noipupdater.service')
-
+print('done')
 print('created and started service')
 
