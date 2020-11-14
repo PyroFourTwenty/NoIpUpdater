2414dd3dbecde3e958a5b62510e4c034c3e5b64a
PyroFourTwenty <kermitthecreeper@gmail.com>
2020-11-14 15:39:11 +0100

    fixed config import issues, IP comparison and waiting time calculation

--- src/SimpleUpdater.py
+++ src/SimpleUpdater.py
@@ -1,5 +1,5 @@
 import requests
-from .config import config
+from src.config import config
 import time
 
 current_ip = ''
@@ -10,7 +10,7 @@ def update_no_ip():
     global current_ip
     try:
         new_ip = requests.get('http://api.ipify.org').text
-        if new_ip is not current_ip:
+        if new_ip != current_ip:
             current_ip = new_ip
             update = requests.get(
                 config['update_url'].format(config['username'], config['password'], config['hostname'], current_ip))
@@ -36,7 +36,12 @@ while running:
     # substract timestamps
     milli_diff = after_task_milli_time - before_task_milli_time
     # subtract passed time from waittime
-    time_to_wait_in_seconds = config['update_interval_in_seconds'] - milli_diff * 1000
-    print('Updating took ', str(milli_diff), 'ms, waiting for ', str(time_to_wait_in_seconds), 'seconds.')
+    time_to_wait_in_seconds = config['update_interval_in_seconds'] - milli_diff / 1000
+    if time_to_wait_in_seconds < 0:
+        print('Updating took longer than your configured interval, waiting for normal interval instead:  ',
+              str(config['update_interval_in_seconds']), 'seconds.')
+        time_to_wait_in_seconds = config['update_interval_in_seconds']
+    else:
+        print('Updating took ', str(milli_diff), 'ms, waiting for ', str(time_to_wait_in_seconds), 'seconds.')
     # wait
     time.sleep(time_to_wait_in_seconds)
